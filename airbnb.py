f43e3d84dbf9a9ac64552f1a717112598db6bcc6
root <root@ip-172-31-36-167.us-west-2.compute.internal>
2018-08-26 17:06:30 +0000

    commiting...

--- airbnb.py
+++ airbnb.py
@@ -2,6 +2,7 @@ import urllib.request
 import json
 from subprocess import call
 import time
+import codecs
 
 header = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}
 user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
@@ -10,7 +11,7 @@ headers={'User-Agent':user_agent,}
 
 request=urllib.request.Request(url,None,headers) #The assembled request
 response = urllib.request.urlopen(request)
-contents = response.read() # The data u need
+contents = response.read().decode('utf-8') # The data u need
 
 parsed = json.loads(contents)
 out = json.dumps(parsed, indent=4, sort_keys=True)
